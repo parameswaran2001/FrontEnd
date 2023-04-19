2304e2729d52859bfce042ace6227b200a6f6838
Govindnair6987 <122169719+Govindnair6987@users.noreply.github.com>
2023-04-19 18:35:09 +0530

    bsdk

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -13,8 +13,8 @@
             case 2: str1="Staff Room A-102";break;
             case 3: str1="Chemistry Lab/Physics Lab A-303";     break;
             case 4: str1="Lecture Hall A-306 CS-2"; break;
-            case 5: str1="Lecture Hall B-203"; break;
-            case 6: str1="AI Lab B-204"; break;
+            case 5: str1="AI Lab B-204"; break;
+            case 6: str1="Lecture Hall B-203"; break;
          }
          switch(DropDownHandler2.sendercopy2)
          {
@@ -23,8 +23,8 @@
             case 2: str2="Staff Room A-102";       break;
             case 3: str2="Chemistry Lab/Physics Lab A-303";     break;
             case 4: str2="Lecture Hall A-306 CS-2"; break;
-            case 5: str2="Lecture Hall B-203"; break;
-            case 6: str2="AI Lab B-204"; break;
+            case 5: str2="AI Lab B-204"; break;
+            case 6: str2="Lecture Hall B-203"; break;
          }
           NavMeshAgent agent = GetComponent<NavMeshAgent>();
           GameObject.Find("Initial location").transform.position=GameObject.Find(str1).transform.position;
