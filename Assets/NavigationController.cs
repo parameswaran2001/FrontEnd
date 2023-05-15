fe29205cbd17c0399f3f37968fb4fccc0de57e6d
Adithya-Anil <73438239+720n@users.noreply.github.com>
2023-05-15 20:01:11 +0530

    ugjjfykb

--- Assets/NavigationController.cs
+++ Assets/NavigationController.cs
@@ -1,5 +1,5 @@
 
-using UnityEngine;
+/*using UnityEngine;
     using UnityEngine.AI;
     
     public class NavigationController : MonoBehaviour 
@@ -18,4 +18,87 @@ using UnityEngine;
            goal.transform.position=GameObject.Find(TargetDestinationDropdown.selectedValue2).transform.position; 
           agent.destination=goal.position; 
        }
+    }*/
+
+ /*using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class NavigationController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    string currentLocation = CurrentLocationDropdown.selectedValue1;
+    string targetDestination = TargetDestinationDropdown.selectedValue2;
+    
+
+    private void Start()
+    {
+        // Assuming you have set up the references to the dropdown menus and the NavMesh agent in the Unity Inspector.
+        agent = GetComponent<NavMeshAgent>();
+        Debug.Log(currentLocation);
+        Debug.Log(targetDestination);
     }
+
+    public void SetAgentDestination()
+    {
+
+
+        // Set the current location of the agent
+        GameObject currentLocationObj = GameObject.Find(currentLocation);
+        if (currentLocationObj != null)
+        {
+            agent.transform.position = currentLocationObj.transform.position;
+        }
+        else
+        {
+            Debug.LogError("Could not find current location: " + currentLocation);
+            return;
+        }
+
+        // Set the destination for the agent
+        GameObject targetDestinationObj = GameObject.Find("Destination");
+        targetDestinationObj.transform.position=GameObject.Find(targetDestination).transform.position;
+
+        if (targetDestinationObj != null)
+        {
+            agent.SetDestination(targetDestinationObj.transform.position);
+        }
+        else
+        {
+            Debug.LogError("Could not find target destination: " + targetDestination);
+        }
+    }
+}*/
+/*using UnityEngine;
+    using UnityEngine.AI;
+    
+    public class PlayerController : MonoBehaviour {
+       
+       public Transform goal;
+
+       void Start () {
+          NavMeshAgent agent = GetComponent<NavMeshAgent>();
+          GameObject.Find("Initial location").transform.position=GameObject.Find(CurrentLocationDropdown.selectedValue1).transform.position;
+          goal.transform.position =GameObject.Find(TargetDestinationDropdown.selectedValue2).transform.position; 
+          agent.destination=goal.position;
+       }
+    }
+*/
+using UnityEngine;
+    using UnityEngine.AI;
+    
+    public class NavigationController : MonoBehaviour {
+       
+       public Transform goal;
+       
+       void Start () {
+        Debug.Log(CurrentLocationDropdown.selectedValue1);
+            Debug.Log(TargetDestinationDropdown.selectedValue2);
+           
+          NavMeshAgent agent = GetComponent<NavMeshAgent>();
+           if(agent!=null){
+          agent.transform.position=GameObject.Find(CurrentLocationDropdown.selectedValue1).transform.position;
+        goal.transform.position =GameObject.Find(TargetDestinationDropdown.selectedValue2).transform.position; 
+          agent.destination = goal.position;}
+      }
+  }
